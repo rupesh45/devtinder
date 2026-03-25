d1a3662b35411eb798f230de49d7a46881eac6fa
Rupesh Kadam <rupeshwork45@gmail.com>
2026-03-25 18:50:32 +0530

    test changes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@ const express = require("express")
 
 const app = express();
 
-app.use("/hello",(req,res) => {
-    res.send("hello from server dashboard fgdf")
-})  
+
+app.get("/user",(req,res) => {
+    res.send({"firstname":"rupesh"})
+})
+app.post("/user",(req,res) => {
+    res.send("saved succesfully")
+})
+app.delete("/user",(req,res) => {
+    res.send("deleted successfully")
+})
+
 app.use("/test",(req,res) => {
     res.send("hello from server test")
 })  
 
-app.use("/",(req,res) => {
-    res.send("hello from home")
-})  
+
 app.listen(3000,() => {
     console.log("Server is up  on 3000...");
 })
