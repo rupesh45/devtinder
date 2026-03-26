1de331a08daf4ef1502dbd33793d81178b33af8f
Rupesh Kadam <rupeshwork45@gmail.com>
2026-03-26 19:12:19 +0530

    rem db

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://rk45:uRSA5Yf1DSPBcvbN@rk45.hd7cgps.mongodb.net/devtinder')
+    
 }
 
 module.exports = connectDB
