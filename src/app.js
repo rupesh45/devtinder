082a4b407bc624bc6642c2509c4d3fef9e38db84
Rupesh Kadam <rupeshwork45@gmail.com>
2026-04-02 17:32:27 +0530

    patch delete and so

--- src/app.js
+++ src/app.js
@@ -24,6 +24,61 @@ app.post('/signup', async (req, res) => {
 
 
 })
+app.patch('/user', async (req, res) => {
+
+    let userId = req.body.userId
+    let data = req.body
+
+    
+    try {
+        const user = await User.findByIdAndUpdate(userId,data,{returnDocument:'before'})
+        console.log(user);
+        res.send("updated")
+        
+    } catch (err) {
+        res.status(400).send('Err saving doc')
+    }
+
+
+
+})
+
+app.get("/user",async (req,res) => {
+    let email = req.body.emailId
+
+    try{
+        let user = await User.find({"emailId" : email})
+        if(user.length === 0 ){
+            res.status(404).send("user not found")
+        }
+        res.send(user)
+    }catch(e){
+        res.status(400).send("something went wrong!")
+    } 
+})
+app.get("/feed",async (req,res) => {
+ 
+    try{
+        let user = await User.find({})
+   
+        res.send(user)
+    }catch(e){
+        res.status(400).send("something went wrong!")
+    } 
+})
+
+app.delete("/user",async (req,res) => {
+
+    let userId = req.body.userId
+
+    try{
+        let user = await User.findByIdAndDelete(userId)
+   
+        res.send('user is now gone!')
+    }catch(e){
+        res.status(400).send("something went wrong!")
+    } 
+})
 
 
 connectDB().then(() => {
